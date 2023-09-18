f08d732a2e5b80e863dfbb5b7406c3508c18ea15
michael-szyszko <michael-szyszko@users.noreply.github.com>
2023-09-18 00:11:33 -0400

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -173,7 +173,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    for (int i = 0; i < pair_count; i++)
+    for (int i = 0; i < pair_count - 1; i++)
     {
         for (int j = 0; j < pair_count - i - 1; j++)
         {
