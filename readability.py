16399e4258e2d6b7d75a8d7de8351d9209d917f1
michael-szyszko <michael-szyszko@users.noreply.github.com>
2023-09-24 22:51:54 -0400

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -7,14 +7,28 @@ length = len(text)
 
 words = text.split(" ")
 word_count = len(words)
-sentence_count = text.count('.') + text.count('?') + text.count('!')
-character_count = length - word_count - sentence_count + 1 - text.count("'") - text.count('"') - text.count(',')
-print(f"word count: {word_count} sentence_count: {sentence_count} character_count: {character_count}")
+sentence_count = text.count(".") + text.count("?") + text.count("!")
+character_count = (
+    length
+    - word_count
+    - sentence_count
+    + 1
+    - text.count("'")
+    - text.count('"')
+    - text.count(",")
+)
+print(
+    f"word count: {word_count} sentence_count: {sentence_count} character_count: {character_count}"
+)
 
 avg_num_letters_per_100_words = character_count / word_count * 100.0
 avg_num_sentences_per_100_words = sentence_count / word_count * 100.0
 
-grade = round((0.0588 * avg_num_letters_per_100_words) - (0.296 * avg_num_sentences_per_100_words) - 15.8)
+grade = round(
+    (0.0588 * avg_num_letters_per_100_words)
+    - (0.296 * avg_num_sentences_per_100_words)
+    - 15.8
+)
 
 if grade > 1 and grade < 16:
     print(f"Grade {grade}")
@@ -22,6 +36,3 @@ elif grade >= MAX_GRADE:
     print(f"Grade {MAX_GRADE}+")
 else:
     print("Before Grade 1")
-
-
-
