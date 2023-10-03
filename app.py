cbf356663a49c54ea16621a754efde21afbfa66d
michael-szyszko <michael-szyszko@users.noreply.github.com>
2023-10-02 22:59:32 -0400

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -37,7 +37,9 @@ def index():
     print(session["user_id"])
     """Show portfolio of stocks"""
     portfolio_holdings = get_portfolio_for_user(session["user_id"])
-    user = get_user_by_userid(session["user_id"])
+    user = get_user_by_userid(int(session["user_id"]))
+    if not user:
+        return apology("user not found", 500)
     cash = user[0]["cash"]
     total_value = cash
 
